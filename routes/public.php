294e6c8e3206133bf9e1300109bcd71a68e95c62
Eddy Loayza <Eddy Loayza>
2016-12-22 18:21:21 -0500

    Commit in lesson 14

--- routes/public.php
+++ routes/public.php
@@ -19,7 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('posts/{post}', [
+Route::get('posts/{post}-{slug}', [
     'uses' => 'PostController@show',
     'as' => 'posts.show',
 ])->where('post', '\d+');
--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -14,16 +14,35 @@ class ShowPostTest extends TestCase
         ]);
 
         $post = factory(\App\Post::class)->make([
-            'title' => 'Como instalar Laravel',
+            'title' => 'Este es el titulo del post',
             'content' => 'Este es el contenido del post'
         ]);
 
         $user->posts()->save($post);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
+    }
+
+    public function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
     }
 }
